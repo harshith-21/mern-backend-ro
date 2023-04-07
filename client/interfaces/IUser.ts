4ce85ec2f63356a00ad2677523c54537e44b8dc4
Harshith Gandhe <gandheharshith21@gmail.com>
2023-04-07 18:16:38 +0530

    first commit

---
+++ client/interfaces/IUser.ts
@@ -0,0 +1,11 @@
+/**
+ * Interface for User schema.
+ */
+export interface IUser {
+    firstname: String,
+    lastname: String,
+    email: String,
+    password: String,
+    aboutme: String,
+    license: String
+}
